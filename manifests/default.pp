c691201d914ba2f2a23a4af8c802441fb8202f1b
Rickard von Essen <rickard.von.essen@gmail.com>
2014-06-13 19:22:19 +0200

    Added build_pipes job

--- manifests/default.pp
+++ manifests/default.pp
@@ -5,14 +5,38 @@ class { 'java':
 class { 'jenkins':
   install_java => false,
 }
+file { '/var/lib/jenkins/jobs/build_pipes/config.xml':
+  source => 'puppet:///modules/config/build_pipes.xml',
+  owner  => 'jenkins',
+  group  => 'jenkins',
+  notify => Service['jenkins'],
+}
 
+package { 'git': }
 package { 'unzip': }
 include gradle
 
 jenkins::plugin {
     "git" : ;
 }
-
+jenkins::plugin {
+    "credentials" : ;
+}
+jenkins::plugin {
+    "git-client" : ;
+}
+jenkins::plugin {
+    "git-parameter" : ;
+}
+jenkins::plugin {
+    "scm-api" : ;
+}
+jenkins::plugin {
+    "ssh-credentials" : ;
+}
+jenkins::plugin {
+    "ssh-slaves" : ;
+}
 jenkins::plugin {
     "job-dsl" : ;
 }
